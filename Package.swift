cd31f82d0616e1340ff96b4abaef78e0450ef4e0
snoe925 <snoe925@gmail.com>
2017-07-28 13:51:24 -0500

    Fix lib list

--- Package.swift
+++ Package.swift
@@ -8,8 +8,7 @@ import PackageDescription
 
 let package = Package(
     name: "Tcl8_6",
-    link: ["tcl8.6", "tclstub8.6", "tclrefcount8.6"],
-    link: "z",
+    link: ["tcl8.6", "tclstub8.6", "tclrefcount8.6", "z"],
     // This C code excluded from the Swift build
     // Build and install this static library with make
     exclude: ["c"]
